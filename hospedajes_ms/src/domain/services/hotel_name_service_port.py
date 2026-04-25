aa0ede2e5bcf6818ea193c46b9e073e1fc279574
David Escobar <de.escobarc1@uniandes.edu.co>
2026-04-24 20:22:21 -0500

    unit tests

--- hospedajes_ms/src/domain/services/hotel_name_service_port.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from uuid import UUID
-
-
-class HotelNameServicePort(ABC):
-    @abstractmethod
-    async def get_hotel_name(self, hotel_id: UUID) -> str | None: ...
